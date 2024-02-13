06c59220e6a462ebb9c2f58bca7b13e98da42204
majanoell <m.noell@protonmail.com>
2024-02-13 22:51:08 +0100

    Fix layer list parameter

--- src/compression_server.cpp
+++ src/compression_server.cpp
@@ -45,7 +45,7 @@ void MapToImage::mapCb(const grid_map_msgs::msg::GridMap& in_msg)
 
   for (const auto& layer : available_layers)
   {
-    if (std::find(layers_.begin(), layers_.end(), layer) != layers_.end() & !layers_.empty())
+    if (std::find(layers_.begin(), layers_.end(), layer) == layers_.end() & !layers_.empty())
       continue;
 
     // The GridMapRosConverter::toCvImage method below assings a value
